233856ffb9e10b3ee22b4bbb4aee71187985f24e
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2018-10-04 21:34:14 -0700

    Fixed sidebar nav JS

--- javascript/MainJS.js
+++ javascript/MainJS.js
@@ -1,9 +1,12 @@
 function openNav() {
     document.getElementById("leftnav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("navButton").style.visibility = "hidden";
 }
 
 function closeNav() {
     document.getElementById("leftnav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("navButton").style.visibility = "visible";
 }
+    
